aa9559e2ce0a8c229cf430929b8bfd282b54ac6c
trytds <1307805140@qq.com>
2020-07-30 20:41:08 +0800

    fix

--- sm2.h
+++ sm2.h
@@ -5,5 +5,63 @@
 //int kdf(unsigned char* x2andy2_byte, int klen, unsigned char* kbuf);
 //
 
-//#include "miracl.h"
-//int initSM2(miracl* pm);
+#include "miracl.h"
+int enrand(unsigned char* r, int rlen);
+int derand(unsigned char* r, int rlen);
+void PrintBuf(unsigned char* buf, int buflen);
+void sm2_keygen(unsigned char* wx, int* wxlen, unsigned char* wy, int* wylen, unsigned char* privkey, int* privkeylen);
+int sm3_z(unsigned char* userid, int userid_len, unsigned char* xa, int xa_len, unsigned char* ya, int ya_len, unsigned char* z);
+int kdf(const char* cdata, int datalen, int keylen, char* retdata);
+void sm2_keyagreement_a1_3(unsigned char* kx1, int* kx1len,
+	unsigned char* ky1, int* ky1len, unsigned char* ra, int* ralen);
+
+int sm2_keyagreement_b1_9(
+	unsigned char* kx1, int kx1len,
+	unsigned char* ky1, int ky1len,
+	unsigned char* pax, int paxlen,
+	unsigned char* pay, int paylen,
+	unsigned char* private_b, int private_b_len,
+	unsigned char* pbx, int pbxlen,
+	unsigned char* pby, int pbylen,
+	unsigned char* ida, int idalen,
+	unsigned char* idb, int idblen,
+	unsigned int  kblen,
+	unsigned char* kbbuf,
+	unsigned char* kx2, int* kx2len,
+	unsigned char* ky2, int* ky2len,
+	unsigned char* xv, int* xvlen,
+	unsigned char* yv, int* yvlen,
+	unsigned char* sb);
+
+int sm2_keyagreement_a4_10(unsigned char* kx1, int kx1len,
+	unsigned char* ky1, int ky1len,
+	unsigned char* pax, int paxlen,
+	unsigned char* pay, int paylen,
+	unsigned char* private_a, int private_a_len,
+	unsigned char* pbx, int pbxlen,
+	unsigned char* pby, int pbylen,
+	unsigned char* ida, int idalen,
+	unsigned char* idb, int idblen,
+	unsigned char* kx2, int kx2len,
+	unsigned char* ky2, int ky2len,
+	unsigned char* ra, int ralen,
+	unsigned int  kalen,
+	unsigned char* kabuf,
+	unsigned char* s1,
+	unsigned char* sa);
+
+void sm2_keyagreement_b10(
+	unsigned char* pax, int paxlen,
+	unsigned char* pay, int paylen,
+	unsigned char* pbx, int pbxlen,
+	unsigned char* pby, int pbylen,
+	unsigned char* kx1, int kx1len,
+	unsigned char* ky1, int ky1len,
+	unsigned char* kx2, int kx2len,
+	unsigned char* ky2, int ky2len,
+	unsigned char* xv, int xvlen,
+	unsigned char* yv, int yvlen,
+	unsigned char* ida, int idalen,
+	unsigned char* idb, int idblen,
+	unsigned char* s2);
+
